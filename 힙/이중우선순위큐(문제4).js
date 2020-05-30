46273853ae453fb5b09ab3f2feaca689e70c1f65
busungg <busungg@naver.com>
2020-05-30 13:53:43 +0900

    알고리즘 테스트(프로그래머스)
      1.  이중우선순위큐(문제4) - 힙

--- 힙/이중우선순위큐(문제4).js
+++ 힙/이중우선순위큐(문제4).js
@@ -1,4 +1,46 @@
+/*
+ * Javascript sort는 mergesort를 사용한다.
+ */
+
 function solution(operations) {
     var answer = [];
+
+    const queue = [];
+    let commands;
+    while(operations.length !== 0) {
+        commands = operations.shift().split(' ');
+        if(commands[0] === 'I') {
+            queue.push(parseFloat(commands[1]));
+
+            if(queue.length > 1) {
+                queue.sort((a,b) => {
+                    return b - a;
+                })
+            }
+        } else {
+            if(queue.length > 0) {
+                switch(parseInt(commands[1])){
+                    case 1:
+                        queue.shift();
+                    break;
+    
+                    case -1:
+                        queue.pop();
+                    break;
+                }
+            }
+            
+        }
+    }
+
+    if(queue.length === 0) {
+        answer = [0,0];
+    } else {
+        answer = [queue[0], queue[queue.length-1]];
+    }
+
     return answer;
 }
+
+console.log(solution(['I 16','D 1']), [0,0]);
+console.log(solution(['I 7','I 5', 'I -5', 'D -1']), [7,5]);
