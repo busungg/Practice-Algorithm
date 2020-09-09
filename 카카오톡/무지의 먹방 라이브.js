3468bf10f89272732a27a57f4534b1d79206faa8
busungg <busungg@naver.com>
2020-09-09 10:01:08 +0900

    카카오 블라인드 2019 1) 무지의 먹방 라이브

--- 카카오톡/무지의 먹방 라이브.js
+++ 카카오톡/무지의 먹방 라이브.js
@@ -1,6 +1,53 @@
 function solution(food_times, k) {
-  var answer = 0;
+  const sortedFoodTimes = food_times
+    .map((time, idx) => {
+      return { time, idx: idx + 1 };
+    })
+    .sort((a, b) => {
+      return a.time - b.time;
+    });
 
+  let elapsedTime = 0,
+    len = 0;
+  for (let i = 0; i < sortedFoodTimes.length; i++) {
+    len = sortedFoodTimes.length - i;
+    elapsedTime =
+      (sortedFoodTimes[i].time - (i === 0 ? 0 : sortedFoodTimes[i - 1].time)) *
+      len;
+    if (k < elapsedTime) {
+      return sortedFoodTimes.slice(i).sort((a, b) => {
+        return a.idx - b.idx;
+      })[k % len].idx;
+    }
+
+    k -= elapsedTime;
+  }
+
+  return -1;
+}
+
+console.log(solution([3, 1, 2], 5));
+console.log(solution([3, 1, 2, 5], 5));
+console.log(solution([1, 1, 1, 1], 5));
+console.log(solution([1, 2], 2));
+
+/**
+ * Rule
+ * 1. 원형 큐
+ * 2. 1초동안 섭취 후 다음 음식을 섭취
+ * 3. 음식 이동시간 없음
+ * 4. 더 섭취할 음식이 없다면 -1 반환
+ *
+ * 알고리즘
+ * 1. 원형 큐 생성 - 배열을 원형 큐로 생각
+ * 2. 음식 남아 있는지 확인
+ * 3. 음식 남아 있으면 -1
+ * 4. 음식 없으면 음식 목록 삭제 다음 음식으로 이동
+ * 5. 모든 음식 없을 시 -1 return
+ */
+
+/**
+  * 정확도 테스트 100% 
   //Init food index
   const foodIdx = [];
   for (let i = 0; i < food_times.length; i++) {
@@ -40,26 +87,4 @@ function solution(food_times, k) {
   }
 
   answer = foodIdx[pos] + 1;
-
-  return answer;
-}
-
-//console.log(solution([3, 1, 2], 5));
-//console.log(solution([3, 1, 2, 5], 5));
-//console.log(solution([1, 1, 1, 1], 5));
-console.log(solution([1, 2], 2));
-
-/**
- * Rule
- * 1. 원형 큐
- * 2. 1초동안 섭취 후 다음 음식을 섭취
- * 3. 음식 이동시간 없음
- * 4. 더 섭취할 음식이 없다면 -1 반환
- *
- * 알고리즘
- * 1. 원형 큐 생성 - 배열을 원형 큐로 생각
- * 2. 음식 남아 있는지 확인
- * 3. 음식 남아 있으면 -1
- * 4. 음식 없으면 음식 목록 삭제 다음 음식으로 이동
- * 5. 모든 음식 없을 시 -1 return
- */
+  */
