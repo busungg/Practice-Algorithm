812be7877709ad0fa41fcd6d1c09024530757915
busungg <busungg@naver.com>
2020-09-19 14:57:21 +0900

    자료구조 트리 1. 수식 트리

--- 자료구조/트리/수식트리.js
+++ 자료구조/트리/수식트리.js
@@ -23,19 +23,92 @@ const isOperand = (token) => {
   return false;
 };
 
-const buildTree = (tokens, node) => {
-  const token = tokens[tokens.length - 1];
-  tokens.length -= 1;
+const buildTree = (tokens) => {
+  if (tokens.length === 0) {
+    return null;
+  }
 
-  if (isOperand(token)) {
-    if (!node.right) {
-      node.right = new Node(token);
+  const root = new Node(tokens.pop());
+  const build = (root, token) => {
+    if (!root) {
+      return null;
     }
 
-    buildTree(tokens, node.right);
+    if (isOperand(root.data)) {
+      if (!build(root.right, token)) {
+        root.right = new Node(token);
+      } else if (!build(root.left, token)) {
+        root.left = new Node(token);
+      }
+    }
+
+    return true;
+  };
+
+  while (tokens.length !== 0) {
+    build(root, tokens.pop());
+  }
+
+  return root;
+};
+
+const inOrder = (root, result) => {
+  if (root.left) {
+    inOrder(root.left, result);
+  }
+
+  result.push(root.data);
+
+  if (root.right) {
+    inOrder(root.right, result);
   }
 };
 
+const calculate = (root, result) => {
+  if (root.left) {
+    calculate(root.left, result);
+  }
+
+  if (root.right) {
+    calculate(root.right, result);
+  }
+
+  if (isOperand(root.data)) {
+    let b = result.pop(),
+      a = result.pop();
+
+    switch (root.data) {
+      case "+":
+        result.push(a + b);
+        break;
+      case "-":
+        result.push(a - b);
+        break;
+      case "/":
+        result.push(a / b);
+        break;
+      case "*":
+        result.push(a * b);
+        break;
+    }
+  } else {
+    result.push(root.data);
+  }
+};
+
+function main() {
+  const root = buildTree([10, 110.5, 11, "+", 1, "/", 10, "-", "+", 5, "*"]);
+  const result = [],
+    calResult = [];
+  inOrder(root, result);
+  calculate(root, calResult);
+
+  console.log(result.join(" "));
+  console.log(calResult[0]);
+}
+
+main();
+
 //{root: null}
-// 10 117.32 11 + 1 / 10 - + 5 *
-// (10 + ( ( 117.32 + 11 ) / 1 ) - 10 )* 5
+// 10 110.5 11 + 1 / 10 - + 5 *
+// (10 + ( ( 110.5 + 11 ) / 1 ) - 10 )* 5
